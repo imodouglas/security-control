c641004263ab9f7fcbc6f0514ddc218fc5697bad
Imo Douglas <imodouglas@gmail.com>
2021-09-11 10:06:06 +0100

    changed database settings to live server

--- classes/database/Db.class.php
+++ classes/database/Db.class.php
@@ -2,9 +2,9 @@
 
     class Db {
         private $dbHost = "localhost";
-        private $dbName = "security_control";
-        private $dbUser = "root";
-        private $dbPass = "";
+        private $dbName = "easywebs_scontrol";
+        private $dbUser = "easywebs_user";
+        private $dbPass = "EW0o9i8u7y6t";
 
         protected function conn(){
             $conn = new PDO('mysql:host='.$this->dbHost.'; dbname='.$this->dbName, $this->dbUser, $this->dbPass);
